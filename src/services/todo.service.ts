3a26a4f24daed8bfbe60b695971f194bb77bc714
Whitey <c1783468@gmail.com>
2024-11-12 23:17:17 +0800

    add service

---
+++ src/services/todo.service.ts
@@ -0,0 +1,10 @@
+import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class TodoService {
+  constructor(/* Add @inject to inject parameters */) {}
+
+  /*
+   * Add service methods here
+   */
+}
